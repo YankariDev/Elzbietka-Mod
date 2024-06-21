ef7a98c0be3029db6e18fab4a6d15ad5a6c5e0a3
YankariDev <109219425+YankariDev@users.noreply.github.com>
2024-06-22 01:41:39 +0200

    Initial

--- src/main/java/net/elzbietkadev/elzbietkamod/ElzbietkaMod.java
+++ src/main/java/net/elzbietkadev/elzbietkamod/ElzbietkaMod.java
@@ -1,6 +1,8 @@
 package net.elzbietkadev.elzbietkamod;
 
 import com.mojang.logging.LogUtils;
+import net.elzbietkadev.elzbietkamod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -28,6 +30,8 @@ public class ElzbietkaMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -48,6 +52,10 @@ public class ElzbietkaMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS)
+        {
+            event.accept(ModItems.SUT_FOOD);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
--- src/main/java/net/elzbietkadev/elzbietkamod/item/ModItems.java
+++ src/main/java/net/elzbietkadev/elzbietkamod/item/ModItems.java
@@ -1,4 +1,21 @@
 package net.elzbietkadev.elzbietkamod.item;
 
+import net.elzbietkadev.elzbietkamod.ElzbietkaMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public class ModItems {
+    public  static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ElzbietkaMod.MOD_ID);
+
+    public static final RegistryObject<Item> SUT_FOOD = ITEMS.register("sut",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
 }
